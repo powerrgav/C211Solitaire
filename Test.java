5fbce6b180d7b96211a8e677b17b0fa8ad04022b
Tarquis Trueshot <brandon.fro.jones@gmail.com>
2023-11-11 00:34:47 -0500

    Add Test class, so we can test classes as we add them. Test.testCard() was added, providing tests for the Card class.

---
+++ Test.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        testCard();
+    }
+
+    //Tests functionality of the Card class.
+
+    private static void testCard(){
+        //Declare local variables.
+        char[] SUITS = {'♣', '♦', '♥', '♠'};
+        ArrayList<Card> deck = new ArrayList<>();
+
+        //Populate deck with cards.
+        for(int value = 1; value < 14; value++){
+            for(char currentSuit : SUITS)
+                deck.add(new Card(value, currentSuit));
+        }
+
+        //Print out cards added to deck.
+        for(Card currentCard : deck)
+            System.out.println(currentCard.getFace());
+    }
+}
